9b47edf5e53ccdbe0b5516387bc2755abfc320e1
shinewaizin <zinwaishine9@gmail.com>
2022-08-04 09:46:35 +0630

    done--favourite--item

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div id="app">
+
+        @include('layouts.nav')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    @vite('resources/js/app.js')
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <h1>Hello !</h1>
+    </div>
+@endsection
